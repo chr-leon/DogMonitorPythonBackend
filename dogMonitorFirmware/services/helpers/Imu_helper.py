4465235df8fc16ea8c16a0fe98899694783c2a65
christianLeon <christian.leon@deepdatat.com>
2022-08-24 22:30:51 -0500

    agrega bulk_create

---
+++ dogMonitorFirmware/services/helpers/Imu_helper.py
@@ -0,0 +1,34 @@
+from services.models import Imu
+
+TIME_INDEX=0
+AX_INDEX=1
+AY_INDEX=2
+AZ_INDEX=3
+GX_INDEX=4
+GY_INDEX=5
+GZ_INDEX=6
+MX_INDEX=7
+MY_INDEX=8
+MZ_INDEX=9
+
+
+def bulk_save(routineId,data,sensorType):
+    listOfRecords = []
+    for record in data:
+        imu = Imu(
+             sampled_at=record[TIME_INDEX],
+             a_x=record[AX_INDEX],
+            a_y=record[AY_INDEX],
+            a_z=record[AZ_INDEX],
+            g_x=record[GX_INDEX],
+            g_y=record[GY_INDEX],
+            g_z=record[GZ_INDEX],
+            m_x=record[MX_INDEX],
+            m_y=record[MY_INDEX],
+            m_z=record[MZ_INDEX],
+            type=sensorType,
+            routineId=routineId
+        )
+        listOfRecords.append(imu)
+    Imu.objects.bulk_create(listOfRecords)
+    print(listOfRecords)
--- dogMonitorFirmware/services/models.py
+++ dogMonitorFirmware/services/models.py
@@ -1,3 +1,26 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Imu(models.Model):
+    id = models.AutoField(primary_key=True)
+    routineId = models.CharField(max_length=200)
+    sampled_at=models.IntegerField()
+
+    a_x=models.FloatField()
+    a_y=models.FloatField()
+    a_z=models.FloatField()
+
+    g_x=models.FloatField()
+    g_y=models.FloatField()
+    g_z=models.FloatField()
+
+    m_x=models.FloatField()
+    m_y=models.FloatField()
+    m_z=models.FloatField()
+
+    type=models.CharField(max_length=100)
+
+    class Meta:
+        db_table="imu"
+        

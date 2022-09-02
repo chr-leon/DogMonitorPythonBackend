25266371eb027c41cda69ed17a1eac71a833f17d
christianLeon <christian.leon@deepdatat.com>
2022-09-02 00:38:51 -0500

    agrga cors

--- dogMonitorFirmware/dogMonitorFirmware/settings.py
+++ dogMonitorFirmware/dogMonitorFirmware/settings.py
@@ -61,6 +61,20 @@ CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:3030',
 # ]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept', 'accept-encoding', 'authorization', 'content-type', 'dnt',
+    'origin', 'user-agent', 'x-csrftoken', 'x-requested-with', 'Authorization'
+]
 #CSRF_COOKIE_HTTPONLY = True
 
 ROOT_URLCONF = 'dogMonitorFirmware.urls'
@@ -102,20 +116,6 @@ environ.Env.read_env()
 # CORS_ALLOW_CREDENTIALS = False
 
 # CORS_REPLACE_HTTPS_REFERER = True
-
-# CORS_ALLOW_METHODS = [
-#     'DELETE',
-#     'GET',
-#     'OPTIONS',
-#     'PATCH',
-#     'POST',
-#     'PUT',
-# ]
-
-# CORS_ALLOW_HEADERS = [
-#     'accept', 'accept-encoding', 'authorization', 'content-type', 'dnt',
-#     'origin', 'user-agent', 'x-csrftoken', 'x-requested-with', 'Authorization'
-# ]
 
 # CORS_EXPOSE_HEADERS = ['authorization', 'Authorization']
 

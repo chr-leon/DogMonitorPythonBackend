59f0492faa580fb8f23d3d7b64b3466249757eb1
christianLeon <christian.leon@deepdatat.com>
2022-09-02 00:55:54 -0500

    allow ip

--- dogMonitorFirmware/dogMonitorFirmware/settings.py
+++ dogMonitorFirmware/dogMonitorFirmware/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*@e9v7hg@_36=2!k(h3#1e-=7fy7e78uuur_e_#6yfbss)0!3-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.1.84']
 
 
 # Application definition

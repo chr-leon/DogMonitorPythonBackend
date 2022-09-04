511c6d58b5072a38b279a0bb86b9660fb7540809
christianLeon <christian.leon@deepdatat.com>
2022-09-03 19:47:00 -0500

    delete routine list view import

--- dogMonitorFirmware/services/api/urls.py
+++ dogMonitorFirmware/services/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from services.api.views import RoutineListView, ServiceViewSet,DeviceViewSet,RoutineViewSet
+from services.api.views import ServiceViewSet,DeviceViewSet,RoutineViewSet
 
 
 app_name="services"

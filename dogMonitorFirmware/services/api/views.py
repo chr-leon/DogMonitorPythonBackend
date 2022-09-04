36000e49ae162ddbea1c08803a732ae676028795
christianLeon <christian.leon@deepdatat.com>
2022-09-03 20:14:27 -0500

    create routine

--- dogMonitorFirmware/services/api/views.py
+++ dogMonitorFirmware/services/api/views.py
@@ -17,9 +17,9 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 # Create your views here.
 class ServiceViewSet(viewsets.ViewSet):
     def routine(self, request):
-        generalPriceSerializer = RoutineSerializer(data=request.data)
-        if not generalPriceSerializer.is_valid():
-            return Response(generalPriceSerializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        serializer = RoutineSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         print(request.data)
         
         # Extract data from request.
@@ -92,3 +92,8 @@ class RoutineViewSet(viewsets.ViewSet):
             filteredRoutines = querySet.filter(Q(name__startswith=searchString))
             serializer = ReadDeviceModelSerializer(filteredRoutines,many=True)
             return Response(serializer.data,status=status.HTTP_200_OK)
+    def create_and_routine(self,request):
+        serializer = RoutineSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data,status=status.HTTP_200_OK)

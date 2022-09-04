3a418561c6285d353b092b8df820b2b0620d1c2a
christianLeon <christian.leon@deepdatat.com>
2022-09-04 00:49:07 -0500

    add samples

--- dogMonitorFirmware/services/api/sensors_serializer.py
+++ dogMonitorFirmware/services/api/sensors_serializer.py
@@ -1,4 +1,4 @@
- from rest_framework import serializers
+from rest_framework import serializers
 from sensors.models import Audio, HeartRate, Imu, Temperature
 
 class ReadImuModelSerializer(serializers.ModelSerializer):

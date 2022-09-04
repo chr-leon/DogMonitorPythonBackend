bfc6b9f3ad5ed49b37ed41d2ffc9fd489c484a2a
christianLeon <christian.leon@deepdatat.com>
2022-09-04 00:57:26 -0500

    get update relations

--- dogMonitorFirmware/services/api/routine_serializers.py
+++ dogMonitorFirmware/services/api/routine_serializers.py
@@ -24,7 +24,7 @@ class ReadRoutineModelSerializer(serializers.ModelSerializer):
         fields = ["id","name","dog_name"]
 
 class ReadRoutineByIdSerializer(serializers.ModelSerializer):
-    imus = ReadImuModelSerializer(many=True)
+    #imus = ReadImuModelSerializer(many=True)
     #temperatures =ReadTemperatureModelSerializer(many=True)
     # heart_rate = ReadHeartRateModelSerializer(many=True)
     # audio = ReadAudioModelSerializer(many=True)
